aaf7887c594ec5ac9c1533c6b45dd96305d624fa
MakinoharaShoko <salufthansa@foxmail.com>
2021-12-25 00:29:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # AniScroll
  Play an animation when whell scrolls.
+
+在滚动滚轮时让元素滑动
+
+## 使用方法
+
+```js
+let ani = new AniScroll();
+ani.init(500);//动画的间隔是500ms
+ani.addElementController('testElement1', 0, 0.7);
+//   参数：
+//1  选择的元素id，
+//2  开始播放动画时已经移动的滚轮距离（0意为一移动滚轮就开始），
+//3  移速
+```
+
---
+++ index.js
@@ -0,0 +1,76 @@
+class AniScroll {
+    constructor() {
+        this.currentPosition = 0;
+        this.elementControlList = [];
+        this.style = '';
+        this.interval = 500;
+    }
+
+    init(interval) {
+        let that = this;
+        document.addEventListener('wheel', function (event) {
+            that.act(event.deltaY)
+        }, false);
+        let style = document.createElement('style');
+        this.style = style;
+        document.head.appendChild(style);
+        setInterval(()=>{this.setElementAni(this)}, interval);
+        this.interval = interval;
+    }
+
+    addElementController(element, startValue, speed) {
+        this.elementControlList.push({
+            elementId: element,
+            currentPosition: 0,
+            previousPosition: 0,
+            startValue: startValue,
+            speed: speed
+        });
+    }
+
+    act(deltaY) {
+        //改变当前的移动位置标准参照状态
+        this.currentPosition = this.currentPosition + deltaY;
+        if (this.currentPosition < 0) this.currentPosition = 0;
+        //遍历elementController,为每个元素设置动画
+        for (let e of this.elementControlList) {
+            //元素开始进入视图
+            if (e.startValue <= this.currentPosition) {
+                e.currentPosition = Math.round(this.currentPosition * e.speed - e.startValue);
+            } else {//元素不在视图内
+                e.currentPosition = 0;
+            }
+        }
+    }
+
+    setElementAni(thisObj) {
+        //遍历所有的元素，计算在这个时间片内的位置改变
+        let styleStr = '\n';
+        // let that = thisObj;
+        for (let e of this.elementControlList) {
+            // let pri = e.previousPosition;
+            let curr = e.currentPosition;
+            //动态创建一个动画 ease-in-out infinite alternate
+            styleStr = styleStr + `
+            #${e.elementId}{
+            animation:${e.elementId + 'Ani'}${e.previousPosition}${e.currentPosition} ${this.interval}ms ease-in-out forwards;
+            -webkit-animation:${e.elementId + 'Ani'}${e.previousPosition}${e.currentPosition} ${this.interval}ms ease-in-out forwards; /* Safari and Chrome */
+            }
+            
+  @keyframes ${e.elementId + 'Ani'}${e.previousPosition}${e.currentPosition} {
+  from {
+    transform: translate(-${e.previousPosition}px, 0);
+  }
+
+  to {
+    transform: translate(-${e.currentPosition}px, 0);
+  }
+}
+`;
+            e.previousPosition = curr;
+        }
+        this.style.innerHTML = styleStr;
+    }
+}
+
+module.exports = AniScroll;
